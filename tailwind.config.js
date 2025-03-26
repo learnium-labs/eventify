17d53f574a7ea0e55724088f4ca492c423fba754
Maman <rohman.banana27@gmail.com>
2025-03-26 21:44:37 +0700

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+// const plugin = require('tailwindcss/plugin');
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+    content: ["./src/**/*.{html,js}", "index.html", "./pages/**/*.{html,js}"],
+    theme: {
+        extend: {
+        container: {
+            // center: true,
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+        },
+        fontFamily: {
+            sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        },
+        },
+    },
+    plugins: [],
+}
